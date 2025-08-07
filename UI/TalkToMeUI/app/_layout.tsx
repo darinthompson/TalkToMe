feae64f96f7e7b7279e343ce6ee287ee67bb03f8
Siddharth Pathak <spathak@student.fullsail.edu>
2025-08-07 17:48:50 -0400

    basic layout for login and register

--- UI/TalkToMeUI/app/_layout.tsx
+++ UI/TalkToMeUI/app/_layout.tsx
@@ -2,6 +2,8 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import LoginScreen from './login';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -11,11 +13,14 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const [loggedIn, setLoggedIn] = useState(false);
 
   if (!loaded) {
-    // Async font loading only occurs in development.
-    //test
     return null;
+  }
+
+  if (!loggedIn) {
+    return <LoginScreen onLogin={() => setLoggedIn(true)} />;
   }
 
   return (
