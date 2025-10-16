db78d45e27e050becd1c59c0d47337945489e539
Siddharth Pathak <spathak@student.fullsail.edu>
2025-10-15 22:28:47 -0400

    adding optimization for mobile devices and functionality to make backend endpoint remote accessible. But it only works if the app allows it

--- talk-to-me-ui/app/password-reset.tsx
+++ talk-to-me-ui/app/password-reset.tsx
@@ -68,7 +68,7 @@ const PasswordReset = () => {
       ? process.env.EXPO_PUBLIC_API_BASE_ANDROID || 'http://10.0.2.2:3001'
       : Platform.OS === 'web'
         ? process.env.EXPO_PUBLIC_API_BASE_WEB || 'http://localhost:3000'
-        : process.env.EXPO_PUBLIC_API_BASE_IOS || 'http://127.0.0.1:3001';
+        : process.env.EXPO_PUBLIC_API_BASE_IOS || 'https://dia-unshrinking-shonda.ngrok-free.dev';
 
   const handleSubmit = async () => {
 
